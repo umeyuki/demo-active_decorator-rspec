936c3fa75f871c2db20cfca3ce44e396f0274630
umeyuki <umeyuki1326+github@gmail.com>
2015-07-30 18:38:05 +0900

    Add skip at spec, add README

---
+++ README.md
@@ -0,0 +1,6 @@
+```
+bundle install
+rake db:create:all
+rake db:migrate
+rspec spec/decorators/user_decorator.rb
+```
--- spec/decorators/user_decorator.rb
+++ spec/decorators/user_decorator.rb
@@ -4,14 +4,16 @@ describe UserDecorator do
 
   context "extend UserDecorator" do
     let!(:user) { User.create(name: "hiromitsu ito", qiita_id: "umeyuki@github").extend(UserDecorator)}
-    subject {  user.extend(UserDecorator).link_to_qiita_profile }
-    it { is_expected.to include "http://qiita.com/umeyuki@github" }
+    skip "ActionView::Helpersのメソッドが使えないため失敗する" do
+      subject {  user.extend(UserDecorator).link_to_qiita_profile }
+      it { is_expected.to include "http://qiita.com/umeyuki@github" }
+    end
   end
 
+  # 成功
   context "active_decorator-rspec" do
     let!(:user) { User.create(name: "hiromitsu ito", qiita_id: "umeyuki@github")}
     subject { decorate( user ).link_to_qiita_profile }
     it { is_expected.to include "http://qiita.com/umeyuki@github" }
   end
-
 end
